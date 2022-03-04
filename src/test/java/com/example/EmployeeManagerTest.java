a40732d992f7c756c0205a1085717b2b6816ee04
Joel <dev.joejoh@gmail.com>
2022-03-04 11:26:23 +0100

    Add testcase for payEmployees catch clause

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -4,44 +4,60 @@ import com.example.mock.EmployeeRepository_Stub;
 import com.example.mock.bankService_stub;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.when;
 
 class EmployeeManagerTest {
     private static EmployeeRepository employeeRepository_stub;
     private static BankService bankService;
+    private static EmployeeRepository employeeRepository_mock;
+    private static BankService bankService_mock = Mockito.mock(BankService.class);
 
     @BeforeAll
     static void setup() {
         employeeRepository_stub = new EmployeeRepository_Stub();
         bankService = new bankService_stub();
+        employeeRepository_mock = Mockito.mock(EmployeeRepository.class);
+        bankService_mock = Mockito.mock(BankService.class);
+        populateRepositoryMock();
+    }
+
+    private static void populateRepositoryMock() {
+        when(employeeRepository_mock.findAll()).thenReturn(
+                List.of(
+                        new Employee("Joel", 2500.00),
+                        new Employee("Kris", 2500.00),
+                        new Employee("Adam", 2500.00),
+                        new Employee("PoPo", 2500.00)
+                ));
     }
 
     @Test
     void paymentsShouldReturn4TransactionsFromStub() {
-        var epm = new EmployeeManager(employeeRepository_stub, bankService);
-        assertThat(epm.payEmployees()).isEqualTo(4);
+        var eManager = new EmployeeManager(employeeRepository_stub, bankService);
+        assertThat(eManager.payEmployees()).isEqualTo(4);
     }
+
     @Test
-    void paymentShouldReturn4TransactionsFromMockitoDoubles(){
-        EmployeeRepository employeeRepository = Mockito.mock(EmployeeRepository.class);
-        BankService bankService = Mockito.mock(BankService.class);
+    void paymentShouldReturn4TransactionsFromMockitoDoubles() {
 
-        when(employeeRepository.findAll()).thenReturn(List.of(
-                new Employee("Joel", 2500.00),
-                new Employee("Kris", 2500.00),
-                new Employee("Adam", 2500.00),
-                new Employee("PoPo", 2500.00)
-        ));
 
-        EmployeeManager eManager = new EmployeeManager(employeeRepository, bankService);
+        EmployeeManager eManager = new EmployeeManager(employeeRepository_mock, bankService_mock);
 
         assertThat(eManager.payEmployees()).isEqualTo(4);
+    }
+
+    @Test
+    void makePaymentWithoutBankServiceShouldNotMakePayments() {
+        EmployeeManager eManager = new EmployeeManager(employeeRepository_mock, null);
+
+        employeeRepository_mock.findAll().forEach((e) -> assertFalse(e.isPaid()));
+        assertThat(eManager.payEmployees()).isEqualTo(0);
+
     }
 }
