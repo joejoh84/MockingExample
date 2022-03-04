239f1300e701e71a089bf5c8d565295f01c1cd76
Joel <dev.joejoh@gmail.com>
2022-03-04 12:54:39 +0100

    Add testcase for set and get methods of Employee

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class EmployeeTest {
 
     @Test
-    void createEmployeeShouldPrintMatchingIdAndSalary() {
+    void createEmployeeShouldPrintMatchingIdAndSalaryTroughToString() {
         String id = "Martha";
         double salary = 39_500.00;
         Employee employee = new Employee(id, salary);
@@ -15,5 +15,24 @@ public class EmployeeTest {
         String expected = "Employee [id=" + id + ", salary=" + salary + "]";
 
         assertThat(employee.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    void valuesSetThroughSetMethodShouldGetSaidValue(){
+        Employee employee = new Employee("", 0.0);
+
+        String id = "Martha";
+        double salary = 39_500.00;
+        boolean isPaid = false;
+
+        employee.setId(id);
+        assertThat(employee.getId()).isEqualTo(id);
+
+        employee.setSalary(salary);
+        assertThat(employee.getSalary()).isEqualTo(salary);
+
+        employee.setPaid(isPaid);
+        assertThat(employee.isPaid()).isEqualTo(isPaid);
+
     }
 }
